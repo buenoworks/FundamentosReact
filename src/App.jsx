1baab7d5263e0ad418f2b3529e892456eb2fed5d
Bueno <aauo@gft.com>
2023-08-16 16:27:31 -0300

    fix: aula 27

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import ComParametro from './components/basics/ComParametro'
 import Fragmento from './components/basics/Fragmento'
 import Aleatorio from './components/basics/Aleatorio'
 import Card from './components/layout/Card'
+import Familia from "./components/basics/Familia"
+import FamiliaMenbro from "./components/basics/FamiliaMenbro"
 
 export default () => (
     <div className="App">
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#05 - Filhos" color="#4682B4">
+                <Familia sobrenome="Bueno">
+                    <FamiliaMenbro nome="André" />
+                    <FamiliaMenbro nome="Joyce" />
+                    <FamiliaMenbro nome="Eva" />
+                </Familia>
+            </Card>
+
             <Card titulo="#04 - Desafio Aleatório" color="#FFC300">
                 <Aleatorio min={1}max={60} />
             </Card>
